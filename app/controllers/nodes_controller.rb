97fdd1f3dee3a8735eaec03818988da1ddd97db0
vagrant <vagrant@vagrant.vm>
2020-07-23 13:42:56 +0000

    [add+fix]nodesコントローラーの修正 #72

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -4,7 +4,8 @@ class NodesController < ApplicationController
   # GET /nodes
   # GET /nodes.json
   def index
-    @nodes = Node.all
+    $post_id = params[:post_id] unless params[:post_id].nil?
+    @nodes = Node.where(post_id: $post_id)
   end
 
   # GET /nodes/1
@@ -12,52 +13,66 @@ class NodesController < ApplicationController
   def show
   end
 
-  def new_a
-    @node = Node.new
-    if $r_source == "a"
-      @nodes = Node.where(parent_id: $parent_id)
-    end
-  end
-
   def new_q
     @node = Node.new
-  end
-
-  # GET /nodes/1/edit
-  def edit
-  end
-
-  # POST /nodes
-  # POST /nodes.json
-  def create_q
-    @node = Node.new(node_params)
-    @node.post_id = $post_id
 
     if $r_source == "a"
       $node_a = Node.last
     end
 
+    @node_a = $node_a
+
+    if flash[:first_time] == true
+      $post_title = Post.find($post_id).title
+      @node_a = nil
+      flash[:first_time] = true
+    end
+    @post_title = $post_title
+  end
+
+
+  def create_q
+    @node = Node.new(node_params)
+    @node.post_id = $post_id
+
     respond_to do |format|
       if @node.save
-        $node_q.add_child(@node)
-        $r_source = "q"
+        $node_a.add_child(@node) unless $node_a.nil?
 
+        if flash[:first_time] == true
+          # そのpost内で初回create_q時にparent_idをnilにする
+          @node.update(parent_id: nil)
+          flash[:first_time] = false
+        end
+        $r_source = "q"
         format.html { redirect_to new_a_nodes_path, notice: 'Node was successfully created.' }
         format.json { render :show, status: :created, location: @node }
       else
-        format.html { render :new }
+        @post_title = $post_title
+        format.html { render new_q_nodes_path }
         format.json { render json: @node.errors, status: :unprocessable_entity }
       end
     end
   end
 
+
+  def new_a
+    @node = Node.new
+
+    if $r_source == "a"
+      @nodes = Node.where(parent_id: $parent_id)
+    elsif $r_source == "q"
+      $node_q = Node.last
+    end
+
+    @post_title = $post_title
+    @node_q = $node_q
+  end
+
+
   def create_a
     @node = Node.new(node_params)
     @node.post_id = $post_id
-    #node_q呼び出し
-    if $r_source == "q"
-      $node_q = Node.last
-    end
 
     respond_to do |format|
       if @node.save
@@ -71,16 +86,28 @@ class NodesController < ApplicationController
           format.json { render :show, status: :created, location: @node }
         elsif params[:commit] == "他の質問を追加"
           format.html { redirect_to new_q_nodes_path, notice: 'Node was successfully created.' }
-          format.json { render :show, status: :created, location: @node }
         else
-          format.html { redirect_to nodes_path, notice: 'Node was successfully created.' }
-          format.json { render :show, status: :created, location: @node }
+          format.html { redirect_to nodes_path(@node.post_id), notice: 'Node was successfully created.' }
         end
       else
-        format.html { render :new }
-        format.json { render json: @node.errors, status: :unprocessable_entity }
+        if params[:commit] == "他の選択肢を追加"
+          @post_title = $post_title
+          @node_q = $node_q
+          @nodes = Node.where(parent_id: $parent_id)
+          format.html { render new_a_nodes_path }
+          format.json { render json: @node.errors, status: :unprocessable_entity }
+        elsif  params[:commit] == "他の質問を追加"
+          format.html { redirect_to new_q_nodes_path, notice: 'Node was successfully created.' }
+        else
+          format.html { redirect_to nodes_path(@node.post_id), notice: 'Node was successfully created.' }
+        end
       end
     end
+  end
+
+
+  # GET /nodes/1/edit
+  def edit
   end
 
   # PATCH/PUT /nodes/1
@@ -89,10 +116,8 @@ class NodesController < ApplicationController
     respond_to do |format|
       if @node.update(node_params)
         format.html { redirect_to @node, notice: 'Node was successfully updated.' }
-        format.json { render :show, status: :ok, location: @node }
       else
         format.html { render :edit }
-        format.json { render json: @node.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -102,7 +127,7 @@ class NodesController < ApplicationController
   def destroy
     @node.destroy
     respond_to do |format|
-      format.html { redirect_to nodes_url, notice: 'Node was successfully destroyed.' }
+      format.html { redirect_to request.referer, notice: 'Node was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         $post_id = @post.id
+        flash[:first_time] = true
         format.html { redirect_to new_q_nodes_path($post_id), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
