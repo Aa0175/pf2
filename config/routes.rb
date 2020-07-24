e5196c46cc7355f21a3b6b5d6f7c4036cbeed172
vagrant <vagrant@vagrant.vm>
2020-07-24 09:28:56 +0000

    [add]Vue.jsの導入 #10

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   resources :answers
   resources :questions
   resources :posts
-  #最終的に問題なければ削除
-  namespace :api do
-    resources :posts, only: %i(show)
-  end
+
   devise_for :users, controllers: {   registrations: 'users/registrations',
                                       sessions: 'users/sessions',
                                       passwords: 'users/passwords'}
   root 'posts#index'
-
   get "users/:id", :to => "users#show", as: "user"
   get "users/", :to => "users#index", as: "users"
 
